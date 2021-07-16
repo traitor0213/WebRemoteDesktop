e190e9d3e78841a4831b187a710fc8e04a5552a9
traitor0213 <32974273+traitor0213@users.noreply.github.com>
2021-07-16 12:31:37 +0900

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.get("/remote-desktop", (req, res) => {
-    res.sendFile(__dirname + "/remote-desktop.html");
-})
-
-app.listen(9001)
